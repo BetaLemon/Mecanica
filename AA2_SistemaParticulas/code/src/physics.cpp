7f79448d41e4a598c1697e97888c854ead6c7081
Unknown <alexwelo@posteo.net>
2018-02-14 13:32:09 +0100

    Partículas que tienen gravedad y rebotan cutremente.
    Falta cambiar la colisión a la que aprendimos en los apuntes (fórmula de física).

--- AA2_SistemaParticulas/code/src/physics.cpp
+++ AA2_SistemaParticulas/code/src/physics.cpp
@@ -1,5 +1,13 @@
 #include <imgui\imgui.h>
 #include <imgui\imgui_impl_sdl_gl3.h>
+#include <glm\vec3.hpp>
+
+struct Particle {
+	glm::vec3 pos;
+	glm::vec3 vel;
+};
+
+Particle particles[SHRT_MAX];	// Esto aquí muy feo.
 
 namespace LilSpheres {															// Parámetros:
 	extern void updateParticles(int startIdx, int count, float* array_data);	// (donde empieza, cuántos elementos, un array así:
@@ -36,11 +44,62 @@ void GUI() {
 void PhysicsInit() {
 	// Do your initialization code here...
 	// ...................................
+
+	//for (int i = 0; i < SHRT_MAX; i++) { particles[i] = { glm::vec3(0, 0, 0), glm::vec3(0, 0, 0)}; }
+	for (int i = 0; i < SHRT_MAX; i++) {
+		glm::vec3 randPos, randVel;
+		randPos = glm::vec3(rand()%5, rand()%5, rand()%5);
+		randVel = glm::vec3(rand() % 10, rand() % 10, rand() % 10);
+		particles[i] = { randPos, randVel };
+	}
 }
 
 void PhysicsUpdate(float dt) {
 	// Do your update code here...
 	// ...........................
+	
+	glm::vec3 gravity = 9.81f * glm::vec3(0, -1, 0);
+	/// PARTICLES:
+	for (int i = 0; i < SHRT_MAX; i++){
+		glm::vec3 newPos, newVel;
+		Particle current = particles[i];
+
+		// Check collision:
+		// en lugar de cutre, habría que usar la formula de la Colision (la d es la de la ecuación del plano)
+		//Cutre:
+		if (current.pos.x > 5 || current.pos.x < -5) {
+			current.vel.x *= -1;
+		}
+		if (current.pos.y > 10 || current.pos.y < 0) {
+			current.vel.y *= -1;
+		}
+		if (current.pos.z > 5 || current.pos.z < -5) {
+			current.vel.z *= -1;
+		}
+
+		newPos = current.pos + dt * current.vel;
+		newVel = current.vel + dt * gravity; // Assuming mass is 1.
+		current.pos = newPos;
+		current.vel = newVel;
+
+	
+
+		particles[i] = current;
+	}
+	
+	// Pasamos del array apto para CPU al array apto para GPU:
+	float particlesGPU[SHRT_MAX * 3];
+	int j = 0;
+	for (int i = 0; i < SHRT_MAX; i++) {
+		particlesGPU[j] = particles[i].pos.x;
+		j++;
+		particlesGPU[j] = particles[i].pos.y;
+		j++;
+		particlesGPU[j] = particles[i].pos.z;
+		j++;
+	}
+	// Actualizamos las partículas:
+	LilSpheres::updateParticles(0, SHRT_MAX, particlesGPU);
 }
 
 void PhysicsCleanup() {
